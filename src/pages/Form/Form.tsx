def30c00f92043223d439e6744d832493a46b0a3
Azamat <lexoice1997@gmail.com>
2023-04-18 21:52:52 +0500

    fix

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -29,7 +29,7 @@ function Form() {
     const data = {
       comment,
       address,
-      user_id: '5356014595',
+      user_id: queryId,
       orders: result,
       total_price: getTotalPrice(order),
     };
