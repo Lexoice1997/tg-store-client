989147f2e5e429f6934cc6024c3c2ebc8227a9f3
Azamat <lexoice1997@gmail.com>
2023-04-19 22:57:55 +0500

    fix

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -35,7 +35,8 @@ function Form() {
     };
     tg.showAlert('Success');
 
-    const sendData = axios.post('https://bot.kvartirabar.uz/order', data).then((res) => onClose());
+    const sendData = await axios.post('https://bot.kvartirabar.uz/order', data);
+
     onClose();
   }, [address, comment, onClose, order, tg, user.id]);
 
