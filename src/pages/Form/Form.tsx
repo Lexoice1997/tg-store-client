582361b0b80cbd01348fc007f4e6a46fe04b9bd5
Azamat <lexoice1997@gmail.com>
2023-04-20 16:32:42 +0500

    fix

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -36,7 +36,7 @@ function Form() {
     // tg.showAlert('Success');
 
     const sendData = axios.post('https://kvartirabar.uz/order', data).then((res) => onClose());
-  }, [address, comment, onClose, order, user.id]);
+  }, [address, comment, onClose, order]);
 
   const navigateToFoodsPage = () => {
     navigate('/');
